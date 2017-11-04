3a295b3a5a92c030e625232842d865c226068e78
Soichi Kamiya <kamiyasoichi@hotmail.co.jp>
2017-11-04 09:03:10 +0000

    11-4

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -10,18 +10,14 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin:     true,
-             activated: true,
-             activated_at: Time.zone.now)
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+               email: email,
+               password:              password,
+               password_confirmation: password)
 end
