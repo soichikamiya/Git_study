5935bd3efeca357286f2f32347c5f0c0c5640424
Soichi Kamiya <kamiyasoichi@hotmail.co.jp>
2017-11-05 03:37:55 +0000

    12-2

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102090934) do
+ActiveRecord::Schema.define(version: 20171105032007) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20171102090934) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
