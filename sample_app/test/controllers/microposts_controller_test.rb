69cf8bebe760835bc375cd1898b97505d77bb5ad
Soichi Kamiya <kamiyasoichi@hotmail.co.jp>
2017-11-07 08:09:35 +0000

    13-4

--- sample_app/test/controllers/microposts_controller_test.rb
+++ sample_app/test/controllers/microposts_controller_test.rb
@@ -13,11 +13,12 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test "should redirect destroy when not logged in" do
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
     assert_no_difference 'Micropost.count' do
-      delete micropost_path(@micropost)
+      delete micropost_path(micropost)
     end
-    assert_redirected_to login_url
+    assert_redirected_to root_url
   end
 end
-
